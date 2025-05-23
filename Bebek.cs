3492c15ff0bd0014f9bff166797b1dc2e48c0073
Bilal Hantik <bilalhantik@gmail.com>
2025-05-23 22:54:26 +0300

    Constructor
    Constructor

---
+++ Bebek.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BebekKişi
+{
+    public class Bebek
+    {
+        public Bebek()
+        {
+            Console.WriteLine("Ingaaaaaaaaaa");
+
+            Ad = "Belirsiz";
+            Soyad = "Belirsiz";
+            DoğumTarihi = DateTime.Now;
+        }
+
+
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DoğumTarihi { get; set; }
+
+        // Parametreli Constructor
+        public Bebek(string ad, string soyad, DateTime dateTime)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            DoğumTarihi = DateTime.Now;
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using BebekKişi;
+
+Bebek yeniDogan1 = new Bebek();
+
+Console.WriteLine("Yeni Doğan bebek bilgileri");
+Console.WriteLine("İsim : " + yeniDogan1.Ad);
+Console.WriteLine("Soyadı : " + yeniDogan1.Soyad);
+Console.WriteLine("Doğum tarihi : " + yeniDogan1.DoğumTarihi);
+
+
+Bebek yeniDogan2 = new Bebek("Ali","Veli", DateTime.Now);
+Console.WriteLine("Yeni Doğan bebek bilgileri");
+Console.WriteLine("İsim : " + yeniDogan2.Ad);
+Console.WriteLine("Soyadı : " + yeniDogan2.Soyad);
+Console.WriteLine("Doğum tarihi : " + yeniDogan2.DoğumTarihi);
+
+
+
